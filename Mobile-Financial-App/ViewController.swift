5c99882ec3c70c3479f206af72c36510d9371517
55487145 <55487145@mm112.ws.ok.ubc.ca>
2018-02-15 22:40:53 -0800

    Instantiate 2 new storyboards

--- Mobile-Financial-App/ViewController.swift
+++ Mobile-Financial-App/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var user: LoginTextField!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     }
