70e3b0f5c0db107474442f4dc2204e526d842090
whfjason <wong.jason@alumni.ubc.ca>
2018-02-16 04:35:49 -0800

    Change UI for expense report view controller & implement pickController

---
+++ Mobile-Financial-App/ExpenseRecordViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ExpenseRecordViewController.swift
+//  Mobile-Financial-App
+//
+//  Created by Jason  Wong on 2018-02-16.
+//  Copyright © 2018 55487145. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseRecordViewController: UIViewController {
+    
+    @IBOutlet weak var expenseReportType: UITextField!
+
+    let account = ["Saving",
+                   "Checking",
+                   "IRA",
+                   "Emergency Fund",
+                   "Insurance",
+                   "Stock Investment"]
+    
+    var selectedAccount: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createAccountPicker()
+        createToolbar()
+
+    }
+    
+    func createAccountPicker() {
+        
+        let accountPicker = UIPickerView()
+        accountPicker.delegate = self
+        expenseReportType.inputView = accountPicker
+    }
+    
+    func createToolbar() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self,
+                                         action: #selector(ExpenseRecordViewController.dismissKeyboard))
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        expenseReportType.inputAccessoryView = toolBar
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension ExpenseRecordViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return account.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return account[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedAccount = account[row]
+        expenseReportType.text = selectedAccount
+    }
+}
