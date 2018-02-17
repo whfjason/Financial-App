ec89dec59ad281b297b2ed3279e9e0489f4890b6
whfjason <wong.jason@alumni.ubc.ca>
2018-02-16 18:16:17 -0800

    Commit and push initial starter project

--- Mobile-Financial-App/RegistrationViewController.swift
+++ Mobile-Financial-App/RegistrationViewController.swift
@@ -6,13 +6,41 @@
 //  Copyright © 2018 55487145. All rights reserved.
 //
 
+// TODO: Implement a successfully login page (aka homepage) and redirect to expense report interface
+// TODO: Move expense report interface out of login button
+// Reminder: Do not push to github! Other terminal may not be compatible with Firebase
+
+import Foundation
 import UIKit
+import Firebase
 
 class RegistrationViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
+    
+    @IBAction func submitButton(sender: UIButton) {
+        signup()
+    }
+    
+    @objc func signup() {
+        guard let email = emailField.text else { return }
+        guard let pwd = passwordField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: pwd) { user, error in
+            if error == nil && user != nil {
+                print("User created")
+            } else {
+                print("Error creating user: \(error!.localizedDescription)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,5 +50,4 @@ class RegistrationViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationItem.title = "Registration"
     }
-
 }
