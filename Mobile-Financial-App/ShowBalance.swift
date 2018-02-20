49603c34542c8b54068dba88888c9be280347b9b
superwendy <wendy@superwendydeair.hitronhub.home>
2018-02-20 12:24:28 -0800

    edit show balance swift file and font change

--- Mobile-Financial-App/ShowBalance.swift
+++ Mobile-Financial-App/ShowBalance.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2018年 55487145. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class fViewController: UIViewController{
+   
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var secondButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        firstButton.buttonDesign()
+        secondButton.buttonDesign()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+extension UIButton{
+    func buttonDesign(){
+        backgroundColor = UIColor.darkGray
+        layer.cornerRadius = frame.height/2
+        setTitleColor(UIColor.white, for: .normal)
+        layer.shadowColor = UIColor.darkGray.cgColor
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+}
+}
