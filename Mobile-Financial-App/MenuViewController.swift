547ec83f76c596dc4cccd9226c8e6e85042358af
21403150 <21403150@mm113.ws.ok.ubc.ca>
2018-03-05 18:05:33 -0800

    Starter UI for menu added

---
+++ Mobile-Financial-App/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Mobile-Financial-App
+//
+//  Created by 21403150 on 2018-03-05.
+//  Copyright © 2018 55487145. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import QuartzCore
+
+class MenuViewController: UIViewController {
+    
+ 
+
+}
+
