058a470dd1866f59a3cd7627ea57cc512231373b
Michael Ren <Michael@michaels-macbook-pro-2.local>
2018-03-31 23:12:45 -0700

    finalization of the functions on password-change interface

--- Mobile-Financial-App/passwordChange.swift
+++ Mobile-Financial-App/passwordChange.swift
@@ -17,6 +17,8 @@ class passwordChange: UIViewController {
     
     @IBOutlet weak var newPwField: UITextField!
     @IBOutlet weak var reNewPwField: UITextField!
+    @IBOutlet weak var curPwField: UITextField!
+    
     
     @IBAction func submitButton(sender: UIButton) {
         changePW()
@@ -24,46 +26,61 @@ class passwordChange: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+    //the tittle of this user interface
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "Update"
     }
     
     func changePW() {
         newPwField.text! = (newPwField.text?.replacingOccurrences(of: " ", with: ""))!
         reNewPwField.text! = (reNewPwField.text?.replacingOccurrences(of: " ", with: ""))!
-
+        curPwField.text! = (curPwField.text?.replacingOccurrences(of: " ", with: ""))!
+        
+        let cPw = curPwField.text!
         let nPw = newPwField.text!
         let rePW = reNewPwField.text!
         
-        
-        
-        if (nPw == rePW) {
-            if(nPw == "" && rePW == ""){
-                let passwordChangedAlert = UIAlertController(title: "null value!", message: "no empty value/space!", preferredStyle: .alert)
-                passwordChangedAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
-                self.present(passwordChangedAlert, animated: true, completion: nil)
-                return
-                
-            }else{
-                Auth.auth().currentUser!.updatePassword(to: nPw) { (error) in
-                    let missMatchAlert = UIAlertController(title: "Password Changed!", message: "Your passwords are successfully", preferredStyle: .alert)
-                    missMatchAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
-                    self.present(missMatchAlert, animated: true, completion: nil)
+        //re-verify the user at the beginning by asking current password
+        Auth.auth().currentUser!.updatePassword(to: cPw) { (error) in
+            if error == nil{
+                //case1: the new password is same as the current one
+                if(cPw == nPw){
+                    let sameAlert = UIAlertController(title: "all the same", message: "Your new password should distinct from the current one", preferredStyle: .alert)
+                    sameAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                    self.present(sameAlert, animated: true, completion: nil)
                     return
-                    
-                    
+                }else if (nPw == rePW) {
+                    //case2.1: the new input is empty
+                    if(nPw == "" && rePW == ""){
+                        let passwordChangedAlert = UIAlertController(title: "null value!", message: "no empty value/space!", preferredStyle: .alert)
+                        passwordChangedAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                        self.present(passwordChangedAlert, animated: true, completion: nil)
+                        return
+                    }else{
+                        //case2.2: password changes successfully
+                        Auth.auth().currentUser!.updatePassword(to: nPw) { (error) in
+                            let missMatchAlert = UIAlertController(title: "Password Changed!", message: "Your passwords are successfully", preferredStyle: .alert)
+                            missMatchAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                            self.present(missMatchAlert, animated: true, completion: nil)
+                            return
+                        }
+                    }
+                } else {
+                    //case2.3: the two times inputs are not match
+                    let passwordChangedAlert = UIAlertController(title: "Password Mismatched!", message: "Your passwords are not matched!", preferredStyle: .alert)
+                    passwordChangedAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                    self.present(passwordChangedAlert, animated: true, completion: nil)
+                    return
                 }
+            }else{
+                //case3: failed verification
+                let cannotVarifyAlert = UIAlertController(title: "Cannot Verify", message: "Please enter the correct current possward", preferredStyle: .alert)
+                cannotVarifyAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                self.present(cannotVarifyAlert, animated: true, completion: nil)
+                return
             }
-        } else {
-            let passwordChangedAlert = UIAlertController(title: "Password Mismatched!", message: "Your passwords are not matched!", preferredStyle: .alert)
-            passwordChangedAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
-            self.present(passwordChangedAlert, animated: true, completion: nil)
-            return
         }
- 
-    
-       
-    
-    
-    }
-    
+        
 }
